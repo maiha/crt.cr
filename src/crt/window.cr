e2e4bea76333c1ad596a76b206ac84676d3b48e9
maiha <maiha@wota.jp>
2016-06-26 03:45:31 +0900

    Window#row,col

--- src/crt/window.cr
+++ src/crt/window.cr
@@ -1,11 +1,15 @@
 # coding: utf-8
 module Crt
   class Window
+    getter row, col
+
     def initialize(r = -1, c = -1, y = 0, x = 0)
       Crt.init
       r = Crt.y if r < 0
       c = Crt.x if c < 0
       @winp = LibNcursesw.newwin(r, c, y, x)
+      @row = r
+      @col = c
     end
 
     def print(y : Int32, x : Int32, str : String)
