434c818951885402d39ca2602bd54474765b730c
Matt <matthewjones@bennington.edu>
2017-04-27 23:00:29 -0400

    Whitespace fixes

--- src/crt.cr
+++ src/crt.cr
@@ -71,53 +71,53 @@ module Crt
       return 4194411
     when '┘' # ACS_LRCORNER
       return 4194410
-	when '├' # ACS_LTEE
-		return 4194420
-	when '┤' # ACS_RTEE
-		return 4194421
-	when '┴' # ACS_BTEE
-		return 4194422
-	when '┬' # ACS_TTEE
-		return 4194423
+    when '├' # ACS_LTEE
+      return 4194420
+    when '┤' # ACS_RTEE
+      return 4194421
+    when '┴' # ACS_BTEE
+      return 4194422
+    when '┬' # ACS_TTEE
+      return 4194423
     when '─' # ACS_HLINE
       return 4194417
     when '│' # ACS_VLINE
       return 4194424
-	when '┼' # ACS_PLUS
-		return 4194414
-	# Can't find equivalent to ACS_S1
-	# Can't find equivalent to ACS_S9
-	when '◆'
-		return 4194400
-	when '▒' # ACS_CKBOARD
-		return 4194401
-	when '°' # ACS_DEGREE
-		return 4194406
-	when '±' # ACS_PLMINUS
-		return 4194407
-	when '•' # ACS_BULLET
-		return 4194430
-	# ACS_LARROW should be covered by the default '<'
-	# ACS_RARROW should be covered by the default '>'
-	# ACS_DARROW should be covered by the default 'v'
-	# ACS_UARROW should be covered by the default '^'
-	# ACS_BOARD  should be covered by the default '#'
-	when '␋' # ACS_LANTERN
-		return 4194409
-	# ACS_BLOCK is the same as ACS_BOARD
-	when '≤' # ACS_LEQUAL
-		return 4194425
-	when '≥' # ACS_GEQUAL
-		return 4194426
-	when 'π' # ACS_PI
-		return 4194427
-	when '≠' # ACS_NEQUAL
-		return 4194428
-	when '£' # ACS_STERLING
-		return 4194429
+    when '┼' # ACS_PLUS
+      return 4194414
+    # Can't find equivalent to ACS_S1
+    # Can't find equivalent to ACS_S9
+    when '◆'
+      return 4194400
+    when '▒' # ACS_CKBOARD
+      return 4194401
+    when '°' # ACS_DEGREE
+      return 4194406
+    when '±' # ACS_PLMINUS
+      return 4194407
+    when '•' # ACS_BULLET
+      return 4194430
+    # ACS_LARROW should be covered by the default '<'
+    # ACS_RARROW should be covered by the default '>'
+    # ACS_DARROW should be covered by the default 'v'
+    # ACS_UARROW should be covered by the default '^'
+    # ACS_BOARD  should be covered by the default '#'
+    when '␋' # ACS_LANTERN
+      return 4194409
+    # ACS_BLOCK is the same as ACS_BOARD
+    when '≤' # ACS_LEQUAL
+      return 4194425
+    when '≥' # ACS_GEQUAL
+      return 4194426
+    when 'π' # ACS_PI
+      return 4194427
+    when '≠' # ACS_NEQUAL
+      return 4194428
+    when '£' # ACS_STERLING
+      return 4194429
     else
       return c.ord
-	end
+    end
   end
 end
 
